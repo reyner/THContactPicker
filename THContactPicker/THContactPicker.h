31239e9110b56cef8f9210f8a74bc5ebe69a2e07
Reyner Crosby <reyner@gmail.com>
2015-12-01 13:08:54 -0800

    Import public headers

--- THContactPicker/THContactPicker.h
+++ THContactPicker/THContactPicker.h
@@ -14,6 +14,7 @@ FOUNDATION_EXPORT double THContactPickerVersionNumber;
 //! Project version string for THContactPicker.
 FOUNDATION_EXPORT const unsigned char THContactPickerVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <THContactPicker/PublicHeader.h>
-
-
+#import "THContactViewStyle.h"
+#import "THContactView.h"
+#import "THContactPickerView.h"
+#import "THContactTextField.h"
